3c2ee5e012caaefdc15e8ba66aa99189fb5347ef
valhuber <valjhuber@gmail.com>
2020-07-22 18:55:10 -0700

    looping ok, but user tables missing

--- nw/build_views/build_views_base.py
+++ nw/build_views/build_views_base.py
@@ -5,32 +5,65 @@ class build_views_base(object):
     """
     Iterate over all tables, create view statements for each
     """
-    _result = "from flask_appbuilder import ModelView\n"
+    _result = "# default view.py\n\n"
 
-    _result += "from flask_appbuilder.models.sqla.interface import SQLAInterface\n"
-
-    _result += "from . import appbuilder, db\n"
-    _result += "from .models import *\n"
+    _indent = "   "
 
     def generate_view(self, db):
 
         tables = db.Model.metadata.tables
+        self._result += self.generate_module_imports()
+
         for each_table in tables.items():
             print(each_table)
             each_result = self.process_each_table(each_table[1])
             self._result += each_result
+        
+        self._result += self.process_add_views(tables)
         return self._result
+
+
 
     def process_each_table(self, each_table):
         table_name = each_table.name
-        if (table_name.startswith("ab_")):
+        if (table_name.startswith("ZZab_")):
             return "# skip admin table: " + table_name + "\n"
         else:
-            table_view_class_definition = "class " + table_name + "ModelView(ModelView):"
-            print("processing table: " + each_table)
-            return table_view_class_definition
+            table_view_class_definition = ""
+            print("processing table: " + table_name)
+            table_view_class_definition += self.generate_class_for_table(
+                each_table)
+            return table_view_class_definition + "\n\n"
+
+
+
+    def generate_module_imports(self):
+        result = "from flask_appbuilder import ModelView\n"
+        result += "from flask_appbuilder.models.sqla.interface import SQLAInterface\n"
+        result += "from . import appbuilder, db\n"
+        result += "from .models import *\n"
+        result += "\n"
+        return result
+
+
+
+    def generate_class_for_table(self, each_table):
+        return self.perform_class_for_table(each_table, "ModelViewSuper")
+
+    def perform_class_for_table(self, each_table, model_name):
+        result = "class " + each_table.name + \
+            model_name + "(" + model_name + "):\n"
+        result += self._indent + "datamodel = SQLAInterface(" + each_table.name + ")\n"
+        return result
+
+
 
     def name_column(self, metadata):
         return "name"
 
 
+
+    def process_add_views(self, tables):
+        result = "\n\n# stub - appbuilder.add_view(...)\n\n"
+        result += "#  " + str(len(tables)) + " pages generated"
+        return result
--- nw/build_views_file.py
+++ nw/build_views_file.py
@@ -11,55 +11,6 @@ from nw.build_views import build_views
 view_builder = build_views.build_views()
 dummy = None
 generated_view = view_builder.generate_view(db)
-print("running, test_col: " + generated_view)
+print("running, views.py: " + generated_view)
 
 log = logging.getLogger(__name__)
-
-DC_RACK_MAX = 20
-ITEM_MAX = 1000
-
-cities = ["Lisbon", "Porto", "Madrid", "Barcelona", "Frankfurt", "London"]
-
-models = ["Server MX", "Server MY", "Server DL380", "Server x440", "Server x460"]
-
-datacenters = list()
-
-
-def get_random_name(names_list, size=1):
-    return names_list[random.randrange(0, len(names_list))]
-
-
-def serial_generator(size=6, chars=string.ascii_uppercase + string.digits):
-    return "".join(random.choice(chars) for _ in range(size))
-
-
-for city in cities:
-    datacenter = Datacenter()
-    datacenter.name = "DC %s" % city
-    datacenter.address = city
-    datacenters.append(datacenter)
-    db.session.add(datacenter)
-    log.info(datacenter)
-    try:
-        db.session.commit()
-        for num in range(1, DC_RACK_MAX):
-            rack = Rack()
-            rack.num = num
-            rack.datacenter = datacenter
-            db.session.add(rack)
-
-    except Exception as e:
-        log.error("Creating Datacenter: %s", e)
-        db.session.rollback()
-
-for i in range(1, ITEM_MAX):
-    item = Item()
-    item.serial_number = serial_generator()
-    item.model = get_random_name(models)
-    db.session.add(item)
-    log.info(item)
-    try:
-        db.session.commit()
-    except Exception as e:
-        log.error("Creating Item: %s", e)
-        db.session.rollback()
