957ab6cb9a8186277333fed311e615961bc43e75
valhuber <valjhuber@gmail.com>
2020-07-26 18:50:28 -0700

    launch config for flask, with hot deploy

--- nw/run.py
+++ nw/run.py
@@ -1,3 +1,26 @@
-from app import app
+from flask import Flask
+from datetime import datetime
+import re
 
-app.run(host="0.0.0.0", port=8080, debug=True)
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello, Flask!"
+
+@app.route("/hello/<name>")
+def hello_there(name):
+    now = datetime.now()
+    formatted_now = now.strftime("%A, %d %B, %Y at %X")
+
+    # Filter the name argument to letters only using regular expressions. URL arguments
+    # can contain arbitrary text, so we restrict to safe characters only.
+    match_object = re.match("[a-zA-Z]+", name)
+
+    if match_object:
+        clean_name = match_object.group(0)
+    else:
+        clean_name = "Friend"
+
+    content = "Hello there, " + clean_name + "! It's " + formatted_now
+    return content
